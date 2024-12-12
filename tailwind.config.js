ec5d9a52b8de899dc35e5459fcf8d4ebc2787bfd
TheGhossst <nandukrishnapkm97@gmail.com>
2024-12-12 20:24:18 +0530

    fix: tailwind.config.js to use tailwindcssAnimate plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -63,5 +64,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
